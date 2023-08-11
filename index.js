5998b39312ab2f390a096f905648404001cdb237
Tiana Lopez <tianadavin@gmail.com>
2023-08-10 18:01:13 -0700

    Adds new element

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+// document.body.append(newHeader);
+newHeader.setAttribute("id", "victory");
+document.querySelector("body").appendChild(newHeader);
+newHeader.textContent = "Tiana is the champion";
