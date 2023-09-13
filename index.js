4ffe11feaba6f2116ec0768fb115f0e5521bd01c
Tiana Lopez <tianadavin@gmail.com>
2023-09-12 18:59:53 -0700

    Add new type of answer

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-// Write your code here!
+// // Write your code here!
 
+// main.remove();
+
+// const newHeader = document.createElement("h1");
+
+// document.querySelector("body").appendChild(newHeader);
+// newHeader.setAttribute("id", "victory");
+// newHeader.textContent = "Tiana is the champion";
+
+
+// // This is more about the order of the test, but both are valid
+// // newHeader.setAttribute("id", "victory");
+// // newHeader.textContent = "Tiana is the champion";
+// // document.body.append(newHeader);
+
+// Second attempt
 main.remove();
-
-const newHeader = document.createElement("h1");
-// document.body.append(newHeader);
-newHeader.setAttribute("id", "victory");
-document.querySelector("body").appendChild(newHeader);
-newHeader.textContent = "Tiana is the champion";
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'TIANA is the champion';
+document.body.append(newHeader);
